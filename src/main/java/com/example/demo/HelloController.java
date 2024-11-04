4ba3b8ade91189ec6c0f7e0952397e642148a607
seghezzi.21139 <seghezzi.michele.studente@itispaleocapa.it>
2024-11-04 13:13:32 +0100

    4

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,13 +2,60 @@ package com.example.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+    public class HelloController {
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+        @FXML
+        private TextField nomeGiocatoreField;
+        @FXML
+        private TextField goalField;
+        @FXML
+        private TextField capitanoField;
+        @FXML
+        private Label infoLabel;
+
+        @FXML
+        protected void aggiungiGiocatore() {
+
+        }
+
+        @FXML
+        protected void visualizzaGiocatori() {
+
+        }
+
+        @FXML
+        protected void modificaDati() {
+
+        }
+
+        @FXML
+        protected void cancellaGiocatore() {
+
+        }
+
+        @FXML
+        protected void visualizzaCapitano() {
+
+        }
+
+        @FXML
+        protected void visualizzaGoalSopra5() {
+
+        }
+
+        @FXML
+        protected void capitanoCasuale() {
+
+
+        }
+
+        @FXML
+        protected void esci() {
+
+            System.exit(0);
+        }
     }
-}
